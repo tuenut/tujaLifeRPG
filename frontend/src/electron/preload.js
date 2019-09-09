06757c5b9b28358060f9fb4c17d9c26457d0578f
--global <groshev41273m@gmail.com>
2019-09-09 18:10:52 +0300

    --

--- frontend/src/electron/preload.js
+++ frontend/src/electron/preload.js
@@ -1,12 +1,6 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 const {remote} = require('electron');
-const zerorpc = require("zerorpc");
-
-
-let client = new zerorpc.Client();
-client.connect("tcp://127.0.0.1:4242");
-
 
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
@@ -20,5 +14,13 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 
+// Use zerorpc to make requests to backend
+const zerorpc = require("zerorpc");
+let client = new zerorpc.Client();
+client.connect("tcp://127.0.0.1:4242");
+
 window.API = {}
+
+// todo: need to use `const ipc = require('electron').ipcMain;` or something like that.
+// todo: beacause granting full access to zerorpc client is bad idea. Need some wrappers for API methods, which returns some data.
 window.API.client = client;
